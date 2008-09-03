218bf7b12b129eefcf3bb0131bf7e37ef518e752
ls <info@example.com>
2008-09-03 17:52:20 +0000

    - Initial commit
    git-svn-id: http://svn.projects.ez.no/ezauthorcontact@1 a22b206e-04fb-48fa-ae02-ebfdbd1f6b83

---
+++ stable/1.0/ezinfo.php
@@ -0,0 +1,14 @@
+<?php
+
+class ezauthorcontactInfo
+{
+    function info()
+    {
+        return array( 'Name' => "eZ Author Contact",
+                      'Version' => "1.0.0",
+                      'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+?>
